7902491e679947d80fe1ba2b62083888018b294a
wuchang <kkang0820qus@gmail.com>
2024-07-18 21:19:56 +0900

    산탄총 추출 리 리팩토링

--- src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_27_move_field/CustomerContract.java
+++ src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_27_move_field/CustomerContract.java
@@ -5,8 +5,18 @@ import java.time.LocalDate;
 public class CustomerContract {
 
     private LocalDate startDate;
+    private double discountRate;
 
-    public CustomerContract(LocalDate startDate) {
+    public CustomerContract(LocalDate startDate, double discountRate) {
         this.startDate = startDate;
+        this.discountRate = discountRate;
     }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
 }
